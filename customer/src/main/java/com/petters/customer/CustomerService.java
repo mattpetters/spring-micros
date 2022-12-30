f2ca92c1f05acf22e025b25552ab366e5f11cf4e
Matt Petters <mcpetters@gmail.com>
2022-12-29 19:38:25 -0500

    svc 1 done

--- customer/src/main/java/com/petters/customer/CustomerService.java
+++ customer/src/main/java/com/petters/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.petters.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
   public void registerCustomer(CustomerRegistrationRequest customerRequest) {
     Customer customer = Customer.builder()
@@ -11,6 +11,8 @@ public record CustomerService() {
         .lastName(customerRequest.lastName())
         .email(customerRequest.email())
         .build();
+
+    customerRepository.save(customer);
 
   }
 }
